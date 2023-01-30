6599d8c8a7488a35095ed73b219cc39c243486fa
unknown <lehoa7439@gmail.com>
2023-01-30 12:42:41 +0700

    Transition to Book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[ show edit update destroy ]
-  
+  before_action :authenticate_user!
   # GET /books or /books.json
   def index
     @books = Book.all
@@ -38,4 +38,23 @@ class BooksController < ApplicationController
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @book.errors, status: :unprocessable_entity }
       end
-	
+    end
+  end
+  # DELETE /books/1 or /books/1.json
+  def destroy
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+      @book = Book.find(params[:id])
+    end
+    # Only allow a list of trusted parameters through.
+    def book_params
+      params.require(:book).permit(:title, :number_of_pages)
+    end
+end
